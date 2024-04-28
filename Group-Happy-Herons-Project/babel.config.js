a0ec10f8a1f692343be7e2597283b6fc0a8c30ea
Xiangnan(Emma) Lu <121429593+ZhuppGalafold@users.noreply.github.com>
2024-04-28 23:36:54 +1200

    merging with estore

--- Group-Happy-Herons-Project/babel.config.js
+++ Group-Happy-Herons-Project/babel.config.js
@@ -1,7 +1,11 @@
 module.exports = function (api) {
-  api.cache(true);
+  api.cache.using(() => process.env.NODE_ENV);
+  const isTest = api.env("test");
   return {
     presets: ["babel-preset-expo"],
-    plugins: [["nativewind/babel"], ["module:react-native-dotenv"]],
+    plugins: [
+      isTest ? [] : ["nativewind/babel"],
+      ["module:react-native-dotenv"],
+    ],
   };
 };
