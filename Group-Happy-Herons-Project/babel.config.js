31ba648f7be439e2c90c1fbb358fdda5d5a02a97
jhin333 <jwen884@aucklanduni.ac.nz>
2024-04-29 21:06:12 +1200

    Modify files

--- Group-Happy-Herons-Project/babel.config.js
+++ Group-Happy-Herons-Project/babel.config.js
@@ -1,17 +1,24 @@
 module.exports = function (api) {
   api.cache.using(() => process.env.NODE_ENV);
   const isTest = api.env("test");
+
+  const plugins = [
+    [
+      "module:react-native-dotenv",
+      {
+        moduleName: "@env",
+        path: ".env",
+      },
+    ],
+  ];
+
+  // Only add this plugin if it's not in the test environment
+  if (!isTest) {
+    plugins.push("nativewind/babel");
+  }
+
   return {
     presets: ["babel-preset-expo"],
-    plugins: [
-      isTest ? [] : ["nativewind/babel"],
-      [
-        "module:react-native-dotenv",
-        {
-          moduleName: "@env",
-          path: ".env",
-        },
-      ],
-    ],
+    plugins: plugins,
   };
 };
